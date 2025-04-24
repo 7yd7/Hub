2bcd43c878d4d24a6fa4d9952068e9b84bcadff6
7yd7 <110498114+7yd7@users.noreply.github.com>
2025-04-24 13:13:30 +0300

    Update NoGame.lua

--- NoGame.lua
+++ NoGame.lua
@@ -105,9 +105,9 @@ Tabs.Main:AddParagraph({
 
 -- لا يوجد ماب
 
-if _G.Script then
-    local success, errorMsg = pcall(function()
-        _G.Script()
+if getgenv().Script then
+    pcall(function() 
+        getgenv().Script() 
     end)
 else
     if Tabs and Tabs.Gameworks then
